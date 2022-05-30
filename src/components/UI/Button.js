dda28a51e2391046ea790ed6db6d7e33a54a34cf
SUMANTI <SUMANTI@DESKTOP-S8MO377>
2022-05-30 10:04:18 +0530

    React-2

---
+++ src/components/UI/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './Button.module.css';
+
+const Button = (props) => {
+    return<button className={classes.button} type={props.type || 'button'} onClick={props.OnClick}>
+    {props.children}
+    </button>
+};
+
+export default Button;
